42d46f6cba4fe11eb5c83485b893f9f37edb88b0
kterna <kterna@users.noreply.github.com>
2026-08-05 12:46:03 +0800

    fix: use relaykit/types in channel_temporary_disable.go

--- model/channel_temporary_disable.go
+++ model/channel_temporary_disable.go
@@ -8,7 +8,7 @@ import (
 	"github.com/QuantumNous/new-api/common"
 	"github.com/QuantumNous/new-api/relaykit/dto"
 	"github.com/QuantumNous/new-api/setting/operation_setting"
-	"github.com/QuantumNous/new-api/types"
+	"github.com/QuantumNous/new-api/relaykit/types"
 )
 
 const DefaultChannelFailureSwitchStatusCodes = "300-599"
